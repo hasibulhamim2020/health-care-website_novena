3abe6ab8b56ac46a7d44474c53e73ea21a191fc3
hasibulhamim2020 <hasibulhamim2020@gmail.com>
2023-09-20 02:46:01 +0600

    backend mastaring done

---
+++ resources/views/admin/include/left-sidebar.blade.php
@@ -0,0 +1,48 @@
+<!--start sidebar -->
+<aside class="sidebar-wrapper">
+    <div class="iconmenu">
+        <div class="nav-toggle-box">
+            <div class="nav-toggle-icon"><i class="bi bi-list"></i></div>
+        </div>
+        <ul class="nav nav-pills flex-column">
+            <li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="right" title="Dashboards">
+                <button class="nav-link" data-bs-toggle="pill" data-bs-target="#pills-dashboards" type="button"><i class="bi bi-house-door-fill"></i></button>
+            </li>
+            <li class="nav-item" data-bs-toggle="tooltip" data-bs-placement="right" title="Application">
+                <button class="nav-link" data-bs-toggle="pill" data-bs-target="#pills-application" type="button"><i class="bi bi-grid-fill"></i></button>
+            </li>
+        </ul>
+    </div>
+    <div class="textmenu">
+        <div class="brand-logo">
+            <img src="{{ asset('admin-assets')}}/assets/images/brand-logo-2.png" width="140" alt=""/>
+        </div>
+        <div class="tab-content">
+            <div class="tab-pane fade" id="pills-dashboards">
+                <div class="list-group list-group-flush">
+                    <div class="list-group-item">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-0">Dashboards</h5>
+                        </div>
+                        <small class="mb-0">Dashboard Project Management</small>
+                    </div>
+                    <a href="index5.html" class="list-group-item"><i class="bi bi-cast"></i>Dashboard</a>
+                    <a href="index4.html" class="list-group-item"><i class="bi bi-archive"></i>Project Management</a>
+                </div>
+            </div>
+            <div class="tab-pane fade" id="pills-application">
+                <div class="list-group list-group-flush">
+                    <div class="list-group-item">
+                        <div class="d-flex w-100 justify-content-between">
+                            <h5 class="mb-0">Categories</h5>
+                        </div>
+                        <small class="mb-0">Manage Categories</small>
+                    </div>
+                    <a href="app-file-manager.html" class="list-group-item"><i class="bi bi-archive"></i>Category Manager</a>
+                    <a href="app-to-do.html" class="list-group-item"><i class="bi bi-check2-square"></i>Category Create</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</aside>
+<!--start sidebar -->
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
-
+///
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
-
+use App\Http\Controllers\DashboardController;
+///
+///
+/////////////Front End Route start /////////////////
 Route::get('/',[HomeController::class,'index'])->name('home');
 Route::get('/about',[HomeController::class,'about'])->name('about');
 Route::get('/service',[HomeController::class,'service'])->name('service');
@@ -14,13 +17,23 @@ Route::get('/appoinment',[HomeController::class,'appoinment'])->name('appoinment
 Route::get('/blog/sidebar',[HomeController::class,'blogSidebar'])->name('blog-sidebar');
 Route::get('/blog/single',[HomeController::class,'blogSingle'])->name('blog-single');
 Route::get('/contact',[HomeController::class,'contact'])->name('contact');
-
+/////////////Front End Route end //////////////////////
+///
+///
+///
+///
+//////////////////////////////////
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
+///
+/////////////Back End Route start ////////////////////
+    Route::get('/dashboard',[DashboardController::class,'index'])->name('dashboard');
+
+/////////////Back End Route end //////////////////////
+///
+///
+///
 });
