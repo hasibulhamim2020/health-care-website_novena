6e893350999f09eafb480b95e77704bf66865657
hasibulhamim2020 <hasibulhamim2020@gmail.com>
2023-09-23 23:31:17 +0600

    23-09-2023

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
     public function index(){
-        return view('front-end.home.home');
+        return view('front-end.home.home',[
+            'blogs'=>Blog::all()
+        ]);
     }
 
     public function about(){
